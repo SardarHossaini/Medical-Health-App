7c6961f04696ca43d33545a1c82635d8ad9ddce9
SardarHossaini <sardarhossaini2022@gmail.com>
2026-03-09 22:55:54 +0430

    feat: desgin the home page

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -56,12 +56,60 @@ class _HomeScreenState extends State<HomeScreen> {
                           decoration: BoxDecoration(color: Colors.white, shape: BoxShape.circle),
                           child: Icon(Icons.add, color: Color(0xFF7165D6), size: 35),
                         ),
+                        SizedBox(height: 35),
+                        Text(
+                          "Clinic Visit",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text("Make an appointment", style: TextStyle(color: Colors.white54)),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 6, spreadRadius: 4)],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFF0EEFA),
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(Icons.home_filled, color: Color(0xFF7165D6), size: 35),
+                        ),
+                        SizedBox(height: 35),
+                        Text(
+                          "Home Visit",
+                          style: TextStyle(
+                            fontSize: 18,
+                            // color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text("Call the doctor home", style: TextStyle(color: Colors.black54)),
                       ],
                     ),
                   ),
                 ),
               ],
             ),
+            SizedBox(height: 25,),
+            
           ],
         ),
       ),
