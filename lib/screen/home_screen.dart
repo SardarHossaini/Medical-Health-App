931ee04f10c12a72ce61c87df93dcaf9db227e54
SardarHossaini <sardarhossaini2022@gmail.com>
2026-03-10 21:09:13 +0430

    feat: complete design of the home page

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -123,9 +123,78 @@ class _HomeScreenState extends State<HomeScreen> {
                 scrollDirection: Axis.horizontal,
                 itemCount: symptoms.length,
                 itemBuilder: (context, index) {
-                  return Container(margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15));
+                  return Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    padding: EdgeInsets.symmetric(horizontal: 25),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF4F6FA),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [BoxShadow(blurRadius: 4, spreadRadius: 2, color: Colors.black12)],
+                    ),
+                    child: Center(
+                      child: Text(
+                        symptoms[index],
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ),
+                  );
                 },
               ),
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Text(
+                "Popular Doctors",
+                style: TextStyle(fontSize: 23, fontWeight: FontWeight.w500, color: Colors.black54),
+              ),
+            ),
+            GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemCount: 4,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [BoxShadow(blurRadius: 4, spreadRadius: 2, color: Colors.black12)],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CircleAvatar(radius: 35, backgroundImage: AssetImage(imgs[index])),
+                        Text(
+                          "Dr. Doctor Name",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        Text("Therapist", style: TextStyle(color: Colors.black45)),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.star, color: Colors.amber),
+                            Text("4.5", style: TextStyle(color: Colors.black45)),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
           ],
         ),
