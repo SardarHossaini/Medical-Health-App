77cbc0eec4069e3057a2f8fd22e6a9cce110818d
SardarHossaini <sardarhossaini2022@gmail.com>
2026-03-11 06:31:20 +0430

    complete the appointment page

--- lib/screen/appointment_screen.dart
+++ lib/screen/appointment_screen.dart
@@ -111,7 +111,147 @@ class AppointmentScreen extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   Text("About Doctor", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                  SizedBox(height: 5),
+                  Text(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.",
+                    style: TextStyle(fontSize: 16, color: Colors.black54),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text("Reviews", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                      SizedBox(width: 10),
+                      Icon(Icons.star, color: Colors.amber),
+                      Text("4.7", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16)),
+                      SizedBox(width: 5),
+                      Text(
+                        "(341)",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                          color: Color(0xFF7165D6),
+                        ),
+                      ),
+                      Spacer(),
+                      TextButton(onPressed: () {}, child: Text("See all")),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 160,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 4,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.all(10),
+                          padding: EdgeInsets.symmetric(vertical: 5),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(blurRadius: 4, spreadRadius: 2, color: Colors.black12),
+                            ],
+                          ),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width / 1.4,
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  leading: CircleAvatar(
+                                    radius: 25,
+                                    backgroundImage: AssetImage(imgs[index]),
+                                  ),
+                                  title: Text(
+                                    "Dr. Doctor Name",
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  subtitle: Text("1 day ago"),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(Icons.star, color: Colors.amber),
+                                      Text("4.7", style: TextStyle(color: Colors.black54)),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.",
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text("Location", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                  ListTile(
+                    leading: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(color: Color(0xFFF0EEFA), shape: BoxShape.circle),
+                      child: Icon(Icons.location_on, color: Color(0xFF7165D6), size: 30),
+                    ),
+                    title: Text(
+                      "Kabul, Afghanistan",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text("Address line of the medical center"),
+                  ),
                 ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(15),
+        height: 130,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [BoxShadow(color: Colors.black12, spreadRadius: 2, blurRadius: 4)],
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Consultation Price", style: TextStyle(color: Colors.black54)),
+                Text(
+                  "\$100",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 12),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0xFF7165D6),
+                ),
+                child: Center(
+                  child: Text(
+                    "Book Appointment",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
